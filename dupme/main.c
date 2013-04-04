16552229200a9eeb7fe13e8c5fa80df34e91df7c
Demid Kucherenko <kucherenkodemid@gmail.com>
2013-04-04 23:36:45 +0400

    exit added

--- dupme/main.c
+++ dupme/main.c
@@ -11,7 +11,25 @@ int parseInt(char* s) {
 	return x;
 }
                    */
-int main (int argc, char * argv[]) {
+
+void writeln() {
+    write(1, "\n", 1);
+}
+int main (int argc, char* argv[]) {
 	int k = atoi(argv[1]);
+	char* buffer = malloc(k + 1);
+	while (true) {
+		int r = read(0, buffer + len, k - len);
+		if (r == 0) {
+			if (len > 0 && cur = 1) {
+	                	writeStr(0, len);
+				writeln;
+				writeStr(0, len);
+				writeln;
+			}
+			return 0;
+		}
+
+	}
 	printf("%d", k);
 }	
